0ab97c7ec9fdb7b00fc931bd9bf871b423337364
ajitofficial <ajitofficial8015@gmail.com>
2022-07-24 04:31:56 +0530

    added functions for adding cart items

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,21 @@
 import React from 'react';
-function Home()
+
+function Home(props)
 {
+    console.warn("Home",props.data)
     return(
         <div>
             <h1>Home Component</h1>
             <div className="add-to-cart">
-            <img src="https://www.shutterstock.com/image-vector/shopping-cart-icon-vector-illustration-1726574749.jpg" />
+                <span className='cart-count'>
+                    ({props.data.length})
+                </span>
+            <img src="https://image.shutterstock.com/shutterstock/photos/259703696/display_1500/stock-vector-shopping-cart-icon-flat-design-with-long-shadows-259703696.jpg" />
             </div>
             <div className="cart-wrapper">
                 <div className="img-wrapper item">
-                    <img src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" />
+                    <img src="https://www.91-img.com/gallery_images_uploads/3/d/3df5ca6a9b470f715b085991144a5b76e70da975.JPG" />
+                    {/* https://bobbyhadz.com/images/blog/react-prevent-multiple-button-clicks/thumbnail.webp */}
                 </div>
                 <div className="text-wrapper item">
                     <span>
@@ -20,7 +26,11 @@ function Home()
                     </span>
                 </div>
                 <div className="btn-wrapper item">
-                    <button>Add To Cart</button>
+                    {/* <button>Add To Cart</button> */}
+                    <button 
+                    onClick={()=>
+                        props.addToCartHandler({price:1000,name:'iphone 11'})
+                    }>Add To Cart</button>
                 </div>
             </div>
         </div>
--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,3 +1,14 @@
+import {connect} from 'react-redux'
 import Home from '../components/Home'
+import {addToCart} from '../Services/Actions/actions'
 
-export default Home;
+const mapStateToProps=state=>({
+    data: state.cardItems
+})
+const mapDispatchToProps=dispatch=>({
+    addToCartHandler:data=>dispatch(addToCart(data))
+})
+
+export default connect(mapStateToProps,mapDispatchToProps)(Home)
+
+// export default Home;
